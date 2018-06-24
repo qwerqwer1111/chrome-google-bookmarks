aa5da8bdfabd11802ac1d8b0e72c1be15cfc458b
qwerqwer1111 <qwerqwer1111+github@gmail.com>
2018-06-24 17:35:38 +0900

    .

--- src/components/Bookmarks.vue
+++ src/components/Bookmarks.vue
@@ -1,15 +1,24 @@
 <template>
   <div>
+    <div v-if="loggedIn">
+      <p><a href="#" @click="selectLabel('')">Clear</a></p>
+      <span v-for="(label, index) in labels">
+        <a href="#" @click="selectLabel(label)">
+          {{ label }}
+        </a>
+        <span v-if="index !== labels.length - 1"> / </span>
+      </span>
+    </div>
     <ul v-if="loggedIn">
       <li v-for="b in bookmarks">
-        <a v-bind:href="b.url" @click="openUrl(b.url)">
+        <a v-bind:href="b.url" @click="openUrl(b.url, false)">
           {{ b.title }}
         </a>: [{{ b.labels.join(',') }}]
       </li>
     </ul>
     <p v-else>
       <a href="http://www.google.com/bookmarks"
-         @click="openUrl('http://www.google.com/bookmarks')">
+         @click="openUrl('http://www.google.com/bookmarks', true)">
         Login
       </a>
     </p>
@@ -28,26 +37,47 @@ export default Vue.extend({
 
   data() {
     return {
-      bookmarks: <Bookmark[]>[],
+      label: '',
+      allBookmarks: <Bookmark[]>[],
       loggedIn: false
     };
   },
 
+  computed: {
+    labels: function (): string[] {
+      const s = new Set<string>();
+      this.allBookmarks.forEach(b => b.labels.forEach(l => s.add(l)));
+      const r = <string[]>[];
+      s.forEach(l => r.push(l));
+      return r.sort();
+    },
+
+    bookmarks: function (): Bookmark[] {
+      if (this.label === '') {
+        return this.allBookmarks;
+      }
+      return this.allBookmarks
+        .filter(b => b.labels.includes(this.label));
+    }
+  },
+
   beforeMount() {
-    findBookmarks()
-      .then(bookmarks => {
-        this.loggedIn = true;
-        this.bookmarks = bookmarks;
-      })
-      .catch(err => {
-        this.loggedIn = false;
-        console.error(err);
-      });
+    findBookmarks().then(bookmarks => {
+      this.loggedIn = true;
+      this.allBookmarks = bookmarks;
+    }).catch(err => {
+      this.loggedIn = false;
+      console.error(err);
+    });
   },
 
   methods: {
-    openUrl(url: string): void {
-      chrome.tabs.create({url});
+    selectLabel(label: string) {
+      this.label = label;
+    },
+
+    openUrl(url: string, active: boolean) {
+      chrome.tabs.create({url, active});
     }
   }
 });
